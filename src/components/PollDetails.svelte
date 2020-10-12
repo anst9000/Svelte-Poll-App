107f7c9235d420d8a444ef9f695808f38ba8f5df
anst9000 <coltlaboy@yahoo.se>
2020-10-12 10:18:36 +0200

    first commit

---
+++ src/components/PollDetails.svelte
@@ -0,0 +1,105 @@
+<script>
+  import Card from '../shared/Card.svelte';
+  import PollStore from '../stores/PollStore.js';
+  import Button from '../shared/Button.svelte';
+  import { tweened } from 'svelte/motion';
+
+  export let poll;
+
+  // Reactive values
+  $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor((poll.votesA / totalVotes) * 100) || 0;
+  $: percentB = Math.floor((poll.votesB / totalVotes) * 100) || 0;
+
+  // tweened percentages
+  const tweenedA = tweened(0);
+  const tweenedB = tweened(0);
+  $: tweenedA.set(percentA);
+  $: tweenedB.set(percentB);
+
+  // handling votes
+  const handleVote = (option, id) => {
+    PollStore.update(currentPolls => {
+      let copiedPolls = [...currentPolls];
+      let upvotedPoll = copiedPolls.find((poll) => poll.id == id);
+
+      if (option === 'a') {
+        upvotedPoll.votesA++;
+      }
+      if (option === 'b') {
+        upvotedPoll.votesB++;
+      }
+
+      return copiedPolls;
+    });
+  }
+
+  // deleting a poll
+  const handleDelete = (id) => {
+    PollStore.update(currentPolls => {
+      return currentPolls.filter(poll => poll.id !== id);
+    });
+  };
+</script>
+
+<Card>
+  <div class="poll">
+    <h3>{ poll.question }</h3>
+    <p>Total votes: { totalVotes }</p>
+    <div class="answer" on:click={ () => handleVote('a', poll.id) }>
+      <div class="percent percent-a" style="width: {$tweenedA}%"></div>
+      <span>{ poll.answerA } ({ poll.votesA })</span>
+    </div>
+    <div class="answer" on:click={ () => handleVote('b', poll.id) }>
+      <div class="percent percent-b" style="width: {$tweenedB}%"></div>
+      <span>{ poll.answerB } ({ poll.votesB })</span>
+    </div>
+    <div class="delete">
+      <Button flat={true} on:click={() => handleDelete(poll.id)}>Delete</Button>
+    </div>
+  </div>
+</Card>
+
+<style>
+  h3 {
+    margin: 0 auto;
+    color: #555;
+  }
+  p {
+    margin-top: 6px;
+    font-size: 14px;
+    color: #aaa;
+    margin-bottom: 30px;
+  }
+  .answer {
+    background: #dcdcdc;
+    cursor: pointer;
+    margin: 10px auto;
+    position: relative;
+  }
+  .answer:hover {
+    opacity: 0.6;
+  }
+  span {
+    display: inline-block;
+    padding: 10px 20px;
+  }
+
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+  .percent-a {
+    border-left: 4px solid rgba(184, 145, 20, 1.0);
+    background: rgba(184, 145, 20, 0.2);
+  }
+  .percent-b {
+    border-left: 4px solid rgba(160, 69, 196, 1.0);
+    background: rgba(160, 69, 196, 0.2);
+  }
+  .delete {
+    margin-top: 30px;
+    text-align: center;
+  }
+</style>
